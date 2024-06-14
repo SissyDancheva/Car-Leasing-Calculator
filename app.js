afb11addbf567afa220362fea9b6af6833cbb70a
SissyManson <komandira123@gmail.com>
2024-06-14 23:37:21 +0300

    Added Input Validation

--- app.js
+++ app.js
@@ -13,14 +13,37 @@ function leasingCalc() {
    // Dynamicly change the text input value based on the slider and vice versa
    carValueSlider.addEventListener('input', () => {
       carValueInput.value = carValueSlider.value;
+      validateInputs();
    });
    carValueInput.addEventListener('input', () => {
       carValueSlider.value = carValueInput.value;
+      validateInputs();
    });
    downPaymentSlider.addEventListener('input', () => {
       downPaymentInput.value = downPaymentSlider.value;
+      validateInputs();
    });
    downPaymentInput.addEventListener('input', () => {
       downPaymentSlider.value = downPaymentInput.value;
+      validateInputs();
+   // Validation of the input fields
+   function validateInputs() {
+      const carVal = Number(carValueInput.value);
+      const downPayment = Number(downPaymentInput.value);
+      const carValError = document.getElementById('carValueError');
+      const downPaymentError = document.getElementById('downPaymentError');
+
+      if (carVal < 10000 || carVal > 200000) {
+         carValError.style.display = 'block';
+      } else {
+         carValError.style.display = 'none';
+      }
+
+      if (downPayment < 10 || downPayment > 50) {
+         downPaymentError.style.display = 'block';
+      } else {
+         downPaymentError.style.display = 'none';
+      }
+   }
    });
 }
